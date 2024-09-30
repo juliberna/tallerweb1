53636e4131114a86f6b84804f44e71eb71d15ff3
TobiasTejerina <tobias.tejerina@gmail.com>
2024-09-30 14:01:45 -0300

    se agregaron funciones para repositorioReview, review y comentario.

--- src/main/java/com/tallerwebi/dominio/model/Publicacion.java
+++ src/main/java/com/tallerwebi/dominio/model/Publicacion.java
@@ -1,28 +1,29 @@
 package com.tallerwebi.dominio.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Calendar;
+import javax.persistence.Column;
+
+import java.time.LocalDateTime;
+
 
 
 public abstract class Publicacion {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Integer id;
+
+    @Column(name = "user_commentary", nullable = false)
     protected String textoComentario;
+    @Column(name = "user_name", nullable = false)
     protected Usuario Usuario;
-    protected Integer likes;
-    protected Integer dislikes;
-    protected Calendar fechaPublicacion;
+    @Column(name = "like", nullable = true)
+    protected Integer likes = 0;
+    @Column(name = "dislike", nullable = true)
+    protected Integer dislikes = 0;
+    @Column(name = "publication_date", nullable = false)
+    protected LocalDateTime fechaPublicacion;
 
-    public Publicacion(com.tallerwebi.dominio.model.Usuario usuario, String textoComentario) {
+    public Publicacion() {
+        fechaPublicacion = LocalDateTime.now();
     }
 
-    public Integer getId() {
-        return id;
-    }
 
     public com.tallerwebi.dominio.model.Usuario getUsuario() {
         return Usuario;
@@ -32,11 +33,31 @@ public abstract class Publicacion {
         return textoComentario;
     }
 
-    public Calendar getFechaPublicacion() {
+    public LocalDateTime getFechaPublicacion() {
         return fechaPublicacion;
     }
 
     public Integer getLikes() {
         return likes;
     }
+
+    public Integer getDislikes() { return dislikes; }
+
+    public void setTextoComentario(String textoComentario) {
+        this.textoComentario = textoComentario;
+    }
+
+    public void setUsuario(com.tallerwebi.dominio.model.Usuario usuario) {
+        Usuario = usuario;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public void setDislikes(Integer dislikes) {
+        this.dislikes = dislikes;
+    }
 }
+
+
