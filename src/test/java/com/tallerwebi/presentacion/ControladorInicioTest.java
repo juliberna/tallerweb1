3fa02ba01b0cc3ffe97e801b8b994c9a04c25e4d
TobiasTejerina <tobias.tejerina@gmail.com>
2024-10-02 15:08:05 -0300

    se agregaron funciones para repositorioReview.

--- src/test/java/com/tallerwebi/presentacion/ControladorInicioTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorInicioTest.java
@@ -1,21 +1,25 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.excepcion.ListaDeReviewsVacias;
+import com.tallerwebi.dominio.excepcion.QueryVacia;
 import com.tallerwebi.dominio.model.Usuario;
-import com.tallerwebi.infraestructura.service.ServicioHome;
+import com.tallerwebi.infraestructura.service.ServicioInicio;
 
-import com.tallerwebi.presentacion.controller.ControladorHome;
+import com.tallerwebi.presentacion.controller.ControladorInicio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
-public class ControladorHomeTest {
+public class ControladorInicioTest {
 
-    ServicioHome servicioHome = mock(ServicioHome.class);
-    ControladorHome controladorInicio = new ControladorHome(servicioHome);
+    ServicioInicio servicioInicio = mock(ServicioInicio.class);
+    ControladorInicio controladorInicio = new ControladorInicio(servicioInicio);
 
     @Test
     public void queSeMuestreElNombreDelUsuarioCorrectamente() {
@@ -23,7 +27,7 @@ public class ControladorHomeTest {
         Usuario usuario = new Usuario();
         usuario.setEmail("hola123@gmail.com");
         usuario.setPassword("hola123");
-
+        //TODO
 
 
 
@@ -31,6 +35,8 @@ public class ControladorHomeTest {
     }
    @Test
     public void queNoSePuedaMostrarUnaListaDeReviewsVacia() {
+
+        //TODO CON MOCKITO
 
    }
 
